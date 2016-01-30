1cb1577b3ca4f8ae0f1b38d6e53b8e010189eb08
ButuzGOL <ButuzGOL@gmail.com>
2016-01-30 10:04:57 +0200

    Endswith polyfill closes #5

---
+++ js/lib/endswith-polyfill.js
@@ -0,0 +1,11 @@
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
