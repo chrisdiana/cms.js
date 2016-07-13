b8e57fb78a5c9428ddbe68933008b107e66f4f83
Byron LK <baddaylk@gmail.com>
2016-07-13 14:03:26 +0100

    Added endsWith() polyfill to stop bug on older browsers
    I use Safari 8, and endsWith() was not working, and I imagine other older browsers have the same issues. This adds the Mozilla developer polyfix, which fixes the problem.

--- js/cms.js
+++ js/cms.js
@@ -7,6 +7,19 @@
  */
 'use strict';
 
+// endsWith polyfill
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
 var CMS = {
 
   settings: {
