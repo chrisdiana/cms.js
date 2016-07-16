d206f95d7d6407ed3170effd5a750c11e54d5620
Byron LK <baddaylk@gmail.com>
2016-07-16 13:00:38 +0100

    New Sitemapping plugin (wip)

---
+++ plugins/sitemapper.cmx.js
@@ -0,0 +1,19 @@
+// Sitemapper creates sitemaps based on local A tags on a page.
+
+__plugin__ = {
+    meta:{
+        "name":"Sitemapper for CMX",
+        "short":"sitemapper",
+        "version":"1.0",
+        "author":"Byron Lewis Kellett",
+        "year":"2016"
+    },
+	pages:[],
+	xml:"",
+    go:function() {
+        // Sitemapper function
+		var ids = new Array();
+		var hrefs = new Array();
+		
+    }
+};
